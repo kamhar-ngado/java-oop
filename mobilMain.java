08218d30c66d66451ab28a3b3c1d7df53dc2056c
kamhar-ngado <kamhar.ngado.id@gmailcom>
2020-02-21 06:48:44 +0700

    update java-oop

--- mobilMain.java
+++ mobilMain.java
@@ -1,6 +1,7 @@
 
 public class mobilMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
         mobil mobil1 = new mobil();
         mobil1.tipe = "Tesla";
@@ -11,10 +12,14 @@ public class mobilMain {
         mobil2.harga = 1400.0;
         mobil2.baterei = 6.0;
 
+
         System.out.println("harga mobil Tesla v1 : " + mobil1.Getharga() + " juta");
         System.out.println("harga mobil Tesla v2 : " + mobil2.Getharga() + " juta");
 
-        System.out.println(mobil1.harga);
+        System.out.println("handsome kamhar");
+	    System.out.println("kamhar selalu handsome as usual");
+	    System.out.println("kamhar memang handsome");
+	
 
 
     }
